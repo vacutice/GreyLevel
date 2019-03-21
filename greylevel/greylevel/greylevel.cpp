b7e8b4a1663be14f1517f9bd11992d753320b498
yongchun weng <2016301750020@whu.edu.cn>
2019-03-21 13:30:02 +0800

    Add files via upload

---
+++ greylevel/greylevel/greylevel.cpp
@@ -0,0 +1,37 @@
+#include <stdio.h>
+int main()
+{
+	int i,j,k;
+	FILE *fp1=NULL,*fp2=NULL;
+	unsigned char pData[256*256];
+	int count[256];
+	float frequence[256];
+	fp1=fopen("F:\\Lena.raw","rb");//ÎÄ¼þ´æÔÚFÅÌ
+	fread(pData,sizeof(unsigned char),256*256,fp1);
+	fclose(fp1);
+	{
+		for (k=0;k<256;k++)
+		{
+			count[k]=0;
+		}
+		for (i=0;i<256;i++)
+		{
+			for (j=0;j<256;j++)
+			{
+				count[pData[i*256+j]]++;
+			}
+		}
+		for (k=0;k<256;k++)
+		{
+			frequence[k]=((float)count[k])/(256*256);
+		}
+	}
+	fp2=fopen("result.txt","w");
+	for (i=0;i<256;i++)
+	{
+		fprintf(fp2,"%d,%d,%f\n",i,count[i],frequence[i]);
+	}
+	
+	fclose(fp2);
+	return 0;
+}
